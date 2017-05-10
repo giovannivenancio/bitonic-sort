f9391805a5632c16058858816e2de76b860e4c8e
Giovanni Venâncio de Souza <gvs11ufpr@gmail.com>
2017-05-10 11:11:51 -0300

    Estrutura de diretorio modificada. Adicionado script bash que testa os algoritmos. Modificado makefile para compilar uma versão com openmp e sem openmp. Desenvolvimento e teste do bitonic sort agora pode ser feito no mesmo arquivo.
    Signed-off-by: Giovanni Venâncio de Souza <gvs11ufpr@gmail.com>

---
+++ experiments/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+mode=$1
+
+BIN_SEQ=../src/bitonic_seq
+BIN_PAR=../src/bitonic_par
+
+if [ "$mode" == "sequential" ]; then
+    echo "*** Running experiments: Sequential Bitonic Sort"
+    echo
+    BIN=$BIN_SEQ
+
+elif [ "$mode" == "parallel" ]; then
+    echo "*** Running experiments: Parallel Bitonic Sort"
+    echo
+    BIN=$BIN_PAR
+else
+    echo "Invalid mode!"
+    exit 0
+fi
+
+for i in `seq 25 27`;
+do
+    time $BIN $((2**$i)) | grep 'N'
+    echo
+done
